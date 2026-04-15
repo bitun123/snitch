df39be25185f52b9312cdecc6faf9b8eb388e227
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-04-16 01:23:03 +0530

    add product model with schema definition for title, description, seller, price, and images

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,48 @@
+import mongoose, { Types } from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,"Please provide a title for this product"],
+        trim:true,
+    },
+    description:{
+        type:String,
+        required:[true,"Please provide a description for this product"],
+    },
+    seller:{
+        Type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:[true,"Please provide a seller for this product"],
+    },
+    price:{
+        amount:{
+            type:Number,
+            required:[true,"Please provide a price for this product"],
+        },
+        currency:{
+            type:String,
+          enum:["USD","EUR","GBP","JPY","CNY","INR"],
+    default:"INR",
+        },
+     
+    },
+    images:[
+        {
+            url:{
+                type:String,
+                required:[true,"Please provide an image url for this product"],
+            },
+            alt:{
+                type:String,
+                required:[true,"Please provide an alt text for this image"],
+            }
+        }
+    ]
+
+},{
+    timestamps:true,
+})
+
+const productModel = mongoose.model("Product",productSchema);
+export default productModel;
