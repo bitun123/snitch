21c8ee80b8574f639b28f1762949890d015a0b53
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-04-18 08:20:35 +0530

    feat: add product creation validation rules and enhance product creation controller with image upload handling

--- backend/src/controller/product.controller.js
+++ backend/src/controller/product.controller.js
@@ -1,15 +1,42 @@
 
 import productModel from "../models/product.model.js";
-
+import { uploadFile } from "../services/storage.services.js";
 
 export const createProductController = async (req, res) => {
 
 try {
+    const {title, description, priceAmount,priceCurrency} = req.body;
+
+    const seller = req.User._id;
+
+
+    const images = await Promise.all(req.files.map(async (file) => {
+        return await uploadFile({buffer:file.buffer,filName:file.originalname})
+    })) 
+
+
+    const product  = await productModel.create({
+        title,
+        description,
+        price:{
+            amount: priceAmount,
+            currency:   priceCurrency || "INR",
+        },
+        seller:seller._id,
+        images
+    })
     
 
-    
+
+    res.status(201).json({
+        message:"Product created successfully",
+        product
+    })
 } catch (error) {
-    
+    res.status(500).json({
+        message:"Error creating product",
+        error
+    })
 }
 
 }
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -2,11 +2,12 @@ import {Router} from 'express';
 import { authenticateSeller } from '../middleware/auth.middleware.js';
 import { createProductController } from '../controller/product.controller.js';
 import { upload } from '../middleware/multer.js';
+import {productCreationValidationRules} from '../validator/product.validator.js';
 
 const productRouter = Router();
 
 
 
-productRouter.post("/", authenticateSeller, upload.array("images", 5), createProductController);
+productRouter.post("/", authenticateSeller,productCreationValidationRules, upload.array("images", 5), createProductController);
 
 export default productRouter;
