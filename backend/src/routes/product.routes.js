9b995debe0ac370c1287caf45d4c015a6262e70f
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-04-18 08:05:40 +0530

    feat: integrate ImageKit for file uploads; add multer middleware for image handling

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import { authenticateSeller } from '../middleware/auth.middleware.js';
 import { createProductController } from '../controller/product.controller.js';
+import { upload } from '../middleware/multer.js';
+
 const productRouter = Router();
 
 
 
-productRouter.post("/", authenticateSeller, createProductController);
+productRouter.post("/", authenticateSeller, upload.array("images", 5), createProductController);
 
 export default productRouter;
---
+++ backend/src/services/storage.services.js
@@ -0,0 +1,25 @@
+import Imagekit from "@imagekit/nodejs";
+import { config } from "../config/config";
+
+
+const client = new Imagekit({
+    privateKey: config.IMAGEKIT_API_KEY,
+})
+
+
+export async function uploadFile({buffer,filName,folder="snitch"}){
+
+
+    try {
+        const result = await client.files.upload({
+            file:await Imagekit.toFile(buffer),
+            fileName:filName,
+            folder:folder
+        })
+
+        return result;
+    } catch (error) {
+        console.error("Error uploading file:", error);
+        throw error;
+    }
+}
